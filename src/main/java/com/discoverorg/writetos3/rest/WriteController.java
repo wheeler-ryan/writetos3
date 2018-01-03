d0a5b7bb307b41520aa3cd903064e252763fdc99
Ryan Wheeler <ryan.wheeler@discoverorg.com>
2018-01-03 10:41:33 -0800

    adding setting ACL

--- src/main/java/com/discoverorg/writetos3/rest/WriteController.java
+++ src/main/java/com/discoverorg/writetos3/rest/WriteController.java
@@ -1,5 +1,7 @@
 package com.discoverorg.writetos3.rest;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.io.WritableResource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,21 +13,27 @@ import java.io.PrintWriter;
 
 @RestController
 public class WriteController {
+    private static final String S3_PREFIX = "s3://";
+    private static final String S3_BUCKET = "dorg-dev-dex";
+    private static final String S3_KEY = "testfile";
     private ResourceLoader resourceLoader;
+    private AmazonS3 amazonS3;
 
-    public WriteController(ResourceLoader resourceLoader) {
+    public WriteController(ResourceLoader resourceLoader, AmazonS3 amazonS3) {
         this.resourceLoader = resourceLoader;
+        this.amazonS3 = amazonS3;
     }
 
     @GetMapping("/write")
     public String writeToS3() {
-        String s3Url = "s3://dorg-dev-dex/testfile";
+        String s3Url = S3_PREFIX + S3_BUCKET + "/" + S3_KEY;
         WritableResource writableResource = (WritableResource) getResourceLoader().getResource(s3Url);
         try (PrintWriter writer = new PrintWriter(writableResource.getOutputStream())) {
             writer.write("This is being written to s3");
         } catch (IOException e) {
             e.printStackTrace();
         }
+        getAmazonS3().setObjectAcl(S3_BUCKET, S3_KEY, CannedAccessControlList.BucketOwnerFullControl);
         return "File written to : " + s3Url;
     }
 
@@ -35,5 +43,13 @@ public class WriteController {
 
     public void setResourceLoader(ResourceLoader resourceLoader) {
         this.resourceLoader = resourceLoader;
+    }
+
+    public AmazonS3 getAmazonS3() {
+        return amazonS3;
+    }
+
+    public void setAmazonS3(AmazonS3 amazonS3) {
+        this.amazonS3 = amazonS3;
     }
 }
